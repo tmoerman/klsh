799f246805b89db40a786489497e3fe776f539e3
Thomas Moerman <thomas.moerman@gmail.com>
2018-08-27 14:36:18 +0200

    fixed demo

--- demo.py
+++ demo.py
@@ -1,4 +1,3 @@
-import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.cross_validation import train_test_split
 from klsh import KernelLSH
@@ -10,10 +9,9 @@ y = data.target
 Xtrain, Xtest, ytrain, ytest = train_test_split(X, y, test_size=0.1,
                                                 random_state=42)
 
-
-klsh = KernelLSH(Xtrain, nbits=8, kernel='rbf', random_state=42)
-
+klsh = KernelLSH(nbits=8, kernel='rbf', random_state=42)
+klsh.fit(Xtrain)
 
 print("Labels on results for 8-bit hash bins of iris data:")
-for i, nbrs in enumerate(klsh.query_top_k(Xtest, 10)):
+for i, nbrs in enumerate(klsh.query(Xtest, 10)):
     print(ytest[i], ytrain[nbrs])
